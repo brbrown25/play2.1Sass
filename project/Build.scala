7dfc2298bd850a2d7ebd7be3fb7d8238f5e8d143
brbrown25 <brandon@bbrownsound.com>
2013-07-18 12:07:06 -0400

    more modifications based on github issues

--- project/Build.scala
+++ project/Build.scala
@@ -2,6 +2,7 @@ import sbt._
 import Keys._
 import play.Project._
 import net.litola.SassPlugin //import sass plugin
+import net.litola.SassPlugin._ //import sass plugin
 
 
 object ApplicationBuild extends Build {
@@ -18,6 +19,10 @@ object ApplicationBuild extends Build {
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
-  ).settings( SassPlugin.sassSettings:_*).settings( SassPlugin.sassOptions := Seq("--compass","-r", "compass","-g") )
+    SassPlugin.sassSettings(0),
+    SassPlugin.sassOptions := Seq("--compass","-r", "compass", "-r"),
+    SassPlugin.sassSettings(2),
+    resourceGenerators in Compile <+= SassPlugin.sassWatcher
+  )//.settings( SassPlugin.sassSettings:_*).settings( SassPlugin.sassOptions := Seq("--compass","-r", "compass","-g") )
 
 }
