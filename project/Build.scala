685b0579573b2354b08deb1346abd905a5e0a768
Brandon Brown <brandon@bbrownsound.com>
2013-07-18 10:24:02 -0400

    added default sass plugin settings
    added line to pull in the default sass plugin settings

--- project/Build.scala
+++ project/Build.scala
@@ -18,6 +18,6 @@ object ApplicationBuild extends Build {
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
-  ).settings( SassPlugin.sassOptions := Seq("--compass","-r", "compass","-g") )
+  ).settings( SassPlugin.sassSettings:_*).settings( SassPlugin.sassOptions := Seq("--compass","-r", "compass","-g") )
 
 }
